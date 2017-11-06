23254dbe75ea859ef5c8f5d7303b45092579a45f
jarnvans <jarnvans@student.arteveldehs.be>
2017-11-06 15:15:42 +0100

    Update Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,18 +2,40 @@ var gulp        = require('gulp');
 var gulpSass    = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
+var gulpBabel = require('gulp-babel');
+
+
 // Static Server & watching scss / html files
 gulp.task('serve', function() {
     browserSync.init({
-        server: './app'
+        server: './app',
+        port: 8080,
+        ui: {
+            port: 8081,
+            weinre: {
+                port: 9081
+            }
+        }
     });
 
     gulp.watch('app/scss/*.scss', ['sass']);
+    gulp.watch('app/*.html').on('change', browserSync.reload)
 });
 
-// Compile sass gitinto CSS
+// Compile sass into CSS
 gulp.task('sass', function() {
     return gulp.src('app/scss/*.scss')
         .pipe(gulpSass())
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
 });
+
+//Compile ES6 into js
+gulp.task('es6', function() {
+    return gulp.src('app/js/**/*.js')
+        .pipe(gulpBabel())
+        .pipe(gulp.dest('app/js/'));
+})
+
+// Default Task
+gulp.task('default', ['serve']);
